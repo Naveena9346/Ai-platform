e4ae9be1731b21f59e918c9dab6f5f238bacc955
Naveena Dudekula <naveenadudekula01@gmail.com>
2026-08-28 11:40:18 +0530

    chore(deps): update package-lock.json & FastAPI responses import

--- nexus_backend/api/v1/chat.py
+++ nexus_backend/api/v1/chat.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sse_starlette.sse import EventSourceResponse
+from fastapi.responses import StreamingResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
